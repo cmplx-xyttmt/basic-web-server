1a079b7b7ba10e1566ecc5a7a47f7d475e7f4632
Isaac Owomugisha <archimedesisaac@gmail.com>
2024-04-22 23:27:21 +0300

    use keyboard interrupt to close socket

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 # Uncomment this to pass the first stage
 import socket
+import signal
+import sys
 
 
 def create_http_response(status, content):
@@ -9,6 +11,7 @@ def create_http_response(status, content):
     response = f"{headers}\r\n{content}\r\n"
     return response
 
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -16,32 +19,37 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    acc_socket, addr_info = server_socket.accept()  # wait for client
+    try:
+        while True:
 
-    request = acc_socket.recv(1024)
-    lines = request.decode().split("\r\n")
-    method, path, protocol = lines[0].split(" ")
-    if path == "/":
-        response_status = "200 OK"
-        headers = f"HTTP/1.1 {response_status}\r\n\r\n"
-        response = headers
-    elif path.startswith("/echo"):
-        path_parts = path.split("/echo/")
-        response = create_http_response("200 OK", path_parts[1])
-    elif path.startswith("/user-agent"):
-        agent = None
-        for line in lines:
-            if line.startswith("User-Agent:"):
-                agent = line.removeprefix("User-Agent: ")
-                break
-        response = create_http_response("200 OK", agent)
-    else:
-        response_status = "404 Not Found"
-        headers = f"HTTP/1.1 {response_status}\r\n\r\n"
-        response = headers
-    acc_socket.send(response.encode())
-    acc_socket.close()
-    server_socket.close()
+            acc_socket, addr_info = server_socket.accept()  # wait for client
+
+            request = acc_socket.recv(1024)
+            lines = request.decode().split("\r\n")
+            method, path, protocol = lines[0].split(" ")
+            if path == "/":
+                response_status = "200 OK"
+                headers = f"HTTP/1.1 {response_status}\r\n\r\n"
+                response = headers
+            elif path.startswith("/echo"):
+                path_parts = path.split("/echo/")
+                response = create_http_response("200 OK", path_parts[1])
+            elif path.startswith("/user-agent"):
+                agent = None
+                for line in lines:
+                    if line.startswith("User-Agent:"):
+                        agent = line.removeprefix("User-Agent: ")
+                        break
+                response = create_http_response("200 OK", agent)
+            else:
+                response_status = "404 Not Found"
+                headers = f"HTTP/1.1 {response_status}\r\n\r\n"
+                response = headers
+            acc_socket.send(response.encode())
+            acc_socket.close()
+    except KeyboardInterrupt:
+        server_socket.close()
+        sys.exit(0)
 
 
 if __name__ == "__main__":
