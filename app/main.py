3423a4f47f99fa5f01cf9717718a1eb6a24b9a23
Isaac Owomugisha <archimedesisaac@gmail.com>
2024-04-22 12:26:09 +0300

    Add handling of content

--- app/main.py
+++ app/main.py
@@ -9,16 +9,27 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    acc_socket, addr_info = server_socket.accept() # wait for client
+    acc_socket, addr_info = server_socket.accept()  # wait for client
 
     request = acc_socket.recv(1024)
     lines = request.decode().split("\r\n")
     method, path, protocol = lines[0].split(" ")
     if path == "/":
         response_status = "200 OK"
+        headers = f"HTTP/1.1 {response_status}\r\n\r\n"
+        response = headers
+    elif path.startswith("/echo"):
+        _, _, random_str = path.split("/")
+        response_status = "200 OK"
+        response_content = random_str
+        headers = (f"HTTP/1.1 {response_status}\r\n"
+                   f"Content-Type: text/plain\r\nContent-Length: {len(response_content)}\r\n")
+        response = f"{headers}\r\n{response_content}\r\n"
     else:
         response_status = "404 Not Found"
-    acc_socket.send(f"HTTP/1.1 {response_status}\r\n\r\n".encode())
+        headers = f"HTTP/1.1 {response_status}\r\n\r\n"
+        response = headers
+    acc_socket.send(response.encode())
     acc_socket.close()
     server_socket.close()
 
